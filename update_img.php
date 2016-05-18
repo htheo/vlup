28141f1e224e19a2aecea2c8d9e4289d0f9c0827
htheo <hinfraytheo@gmail.com>
2016-05-18 20:17:32 +0200

    fonction update

---
+++ update_img.php
@@ -0,0 +1,37 @@
+<?php
+
+//Copie de l'avatar dans le dossier image
+include('config.php');
+	if(!isset($_SESSION["role"])){
+
+		header("Location: erreur_acces.php");
+	}else{
+
+	if(empty(file_get_contents($_FILES['avatar']['tmp_name']))){
+		header("Location: erreur_acces.php");
+	}
+	$title		= htmlentities($_POST['title']);
+	$id		= htmlentities($_POST['suppression']);
+	$type	= htmlentities($_POST['type']);
+	$validation	= htmlentities($_POST['validation']);
+	$description	= htmlentities($_POST['description']);
+	$description = str_replace("\n","<br/>",$description);
+
+
+
+$sql = 'UPDATE vlup_articles2 
+SET title= "'.$title.'",
+	validation= "'.$validation.'",
+	type= "'.$type.'",
+	description= "'.$description.'"
+
+WHERE ID="'.$id.'"';
+$db->exec($sql);
+
+
+header("Location: index.php");
+
+	
+
+}
+?>
